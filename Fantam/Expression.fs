454adcb4ac56346b75fbb8887fbed1e37aa959b1
CraigStuntz <craig_stuntz@acm.org>
2014-04-23 00:00:15 -0400

    Tidy

--- Fantam/Expression.fs
+++ Fantam/Expression.fs
@@ -22,18 +22,11 @@
     | Prefix  (_, operand)                  -> errorsFor operand
 
     let rec print = function
-    | Assign (name, right) -> 
-        sprintf "(%s = %s)" name (print right)
-    | Call   (func, args)  -> 
-        sprintf "%s(%s)" (print func) (System.String.Join(", ", List.map print args))
-    | Conditional (ifExp, thenExp, elseExp) -> 
-        sprintf "(%s ? %s : %s)" (print ifExp) (print thenExp) (print elseExp)
-    | Error message -> sprintf "Error: %A" message
-    | Name   name -> 
-        name
-    | Operator (left, operator, right) ->
-        sprintf "(%s %s %s)" (print left) (printToken operator) (print right)
-    | Postfix (left, operator) ->
-        sprintf "(%s%s)" (print left) (printToken operator)
-    | Prefix  (operator, operand) ->
-        sprintf "(%s%s)" (printToken operator) (print operand)
+    | Assign (name, right)                  -> sprintf "(%s = %s)" name (print right)
+    | Call   (func, args)                   -> sprintf "%s(%s)" (print func) (System.String.Join(", ", List.map print args))
+    | Conditional (ifExp, thenExp, elseExp) -> sprintf "(%s ? %s : %s)" (print ifExp) (print thenExp) (print elseExp)
+    | Error message                         -> sprintf "Error: %A" message
+    | Name   name                           -> name
+    | Operator (left, operator, right)      -> sprintf "(%s %s %s)" (print left) (printToken operator) (print right)
+    | Postfix (left, operator)              -> sprintf "(%s%s)" (print left) (printToken operator)
+    | Prefix  (operator, operand)           -> sprintf "(%s%s)" (printToken operator) (print operand)
